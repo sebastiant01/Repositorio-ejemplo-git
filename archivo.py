8b9e9ff8f026357b3d6bd32d52a35b251cac3786
Sebastián Torres <sebastiantorres1129733@correo.itm.edu.co>
2026-02-05 15:52:27 -0500

    feat: Se creó la función sumar_dos_numeros con su respectivo docstring.

---
+++ archivo.py
@@ -0,0 +1,19 @@
+def sumar_dos_numeros(num1: int, num2: int) -> int:
+    """
+    Suma dos números enteros y devuelve el resultado.
+
+    Args:
+        num1 (int): Primer número entero.
+        num2 (int): Segundo número entero.
+
+    Returns:
+        int: La suma de `num1` y `num2`.
+
+    Examples:
+        >>> sumar_dos_numeros(3, 5)
+        8
+    """
+    return num1 + num2
+
+
+print(sumar_dos_numeros(2, 3))
